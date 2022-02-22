2b5b7520913c2546df99936b85d54a7daf5fc56a
hshe <hshe@163.com>
2022-02-22 14:04:54 +0800

    fix:update domain

--- conf/conf.go
+++ conf/conf.go
@@ -1,35 +1,35 @@
 package conf
 
 type PcsResponseBase struct {
-	ErrorCode int  	 `json:"error_code"`
+	ErrorCode int    `json:"error_code"`
 	ErrorMsg  string `json:"error_msg"`
 	RequestID uint64 `json:"request_id"`
 }
 
 type CloudDiskResponseBase struct {
-	ErrorCode int  	 `json:"errno"`
+	ErrorCode int    `json:"errno"`
 	ErrorMsg  string `json:"errmsg"`
 	RequestID uint64 `json:"request_id"`
 }
 
 type TestDataConfig struct {
-	ClientID string
-	ClientSecret string
-	RedirectUri string
-	Code string
-	AccessToken string
-	RefreshToken string
-	Dir string
-	FsID uint64
-	Path string
-	LocalFilePath string
+	ClientID        string
+	ClientSecret    string
+	RedirectUri     string
+	Code            string
+	AccessToken     string
+	RefreshToken    string
+	Dir             string
+	FsID            uint64
+	Path            string
+	LocalFilePath   string
 	TranscodingType string
 }
 
 const (
 	BaiduOpenApiDomain = "https://openapi.baidu.com"
-	OpenApiDomain = "https://pan.baidu.com"
-	PcsDataDomain = "https://d.pcs.baidu.com"
+	OpenApiDomain      = "https://pan.baidu.com"
+	PcsDataDomain      = "https://c.pcs.baidu.com"
 )
 
 // 测试参数
